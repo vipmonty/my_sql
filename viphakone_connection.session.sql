7445d7a101accc55dcac48eca173f485bf3b7c4d
Viphakone Monty <10835390@my.uvu.edu>
2023-08-08 10:49:38 -0600

    updated on 8/8/23

--- viphakone_connection.session.sql
+++ viphakone_connection.session.sql
@@ -1,5 +1,16 @@
-CREATE TABLE student(
-    student_id int primary key auto_increment,
-    name varchar(24),
-    major varchar(24)
-);
+-- find out who is the manager of
+-- the person with the most sales
+
+-- tables:
+-- employee
+-- works_with
+-- branch
+
+
+-- SELECT emp_id, sum(total_sales) FROM works_with
+-- GROUP BY emp_id
+-- ORDER BY sum(total_sales) DESC LIMIT 1
+
+
+SELECT * FROM employee
+WHERE emp_id = 102
