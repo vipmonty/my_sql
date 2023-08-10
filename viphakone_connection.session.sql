1576b02b3e53b5cf994c070ad668ebb3dd5589a9
Viphakone Monty <10835390@my.uvu.edu>
2023-08-10 10:49:50 -0600

    updated on 8-9-23 @ 10:49 am

--- viphakone_connection.session.sql
+++ viphakone_connection.session.sql
@@ -1,16 +1,6 @@
--- find out who is the manager of
--- the person with the most sales
+-- using the sql_store.orders
+-- table and update comments column
+-- to 'Gold' to customers who have more
+-- than 3000 points
 
--- tables:
--- employee
--- works_with
--- branch
-
-
--- SELECT emp_id, sum(total_sales) FROM works_with
--- GROUP BY emp_id
--- ORDER BY sum(total_sales) DESC LIMIT 1
-
-
-SELECT * FROM employee
-WHERE emp_id = 102
+use sql_store
